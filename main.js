fcd568634d3fc1a1beb539dc5dc25a71d39d88d5
naylalaver <113601042+naylalaver@users.noreply.github.com>
2024-05-23 17:24:16 -0300

    aaaaaaa

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-26T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-24T00:00:00");
 const tempoObjetivo2 = new Date("2032-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2028-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
